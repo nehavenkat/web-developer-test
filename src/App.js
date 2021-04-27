5c788f86a3c72075e190f22bade17fc511c5d2b0
neha venkat <venkatsetty.neha@gmail.com>
2021-04-27 10:58:06 +0200

    updated all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MainComponent from './Components/MainComponent';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import MovieDetails from './Components/MovieDetails';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+                <Route path="/" exact component={MainComponent}/>
+                <Route path="/moviedetails/:movieId" component={MovieDetails}/>
+            </Router>
+
     </div>
   );
 }
